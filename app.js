6c42441ae150fedf54d139f22b0eb66075c7e8ae
dominique Taplin <Dtaplin21@aol.com>
2024-03-31 18:04:52 -0700

    fix bugs

--- app.js
+++ app.js
@@ -5,23 +5,25 @@ const blockHeight = 20;
 class Block {
     constructor(xAxis, yAxis) {
         this.bottomLeft = [xAxis, yAxis];
-        this.bottomRight = [xAxis, bottomWidth, yAxis]
-        this.topLeft = [xAxios, yAxis + blockHeight ]
-        this.topRight = [xAxis, + blockwidth, yAxis + blockHeight]
+        this.bottomRight = [xAxis, blockWidth, yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRight = [xAxis, + blockWidth, yAxis + blockHeight];
     }
 }
 
 const blocks = [
-    new Block = (10, 270)
+    new Block(10, 270)
 ]
 
 
 function addBlock() {
+    for(let i = 0; i < blocks.length; i++) {
 const block = document.createElement('div');
 block.classList.add('block');
-block.style.left = "100px";
+block.style.left = blocks[i].bottomLeft[0]
 block.style.bottom = '50px';
 grid.appendChild(block)
+    }
 }
 
 addBlock()
